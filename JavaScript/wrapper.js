2e3ba5bc89abfa0ff30ab176ece3112c7a138718
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-08 07:37:51 +0300

    Use tick templates

--- JavaScript/wrapper.js
+++ JavaScript/wrapper.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const wrapFunction = (key, fn) => {
-  console.log('Wrap function: ' + key);
+  console.log(`Wrap function: ${key}`);
   return (...args) => {
-    console.log('Called wrapper for: ' + key);
+    console.log(`Called wrapper for: ${key}`);
     console.dir({ args });
     if (args.length > 0) {
       let callback = args[args.length - 1];
       if (typeof callback === 'function') {
         args[args.length - 1] = (...args) => {
-          console.log('Callback: ' + key);
+          console.log(`Callback: ${key}`);
           callback(...args);
         };
       } else {
         callback = null;
       }
     }
-    console.log('Call: ' + key);
+    console.log(`Call: ${key}`);
     console.dir(args);
     const result = fn(...args);
-    console.log('Ended wrapper for: ' + key);
+    console.log(`Ended wrapper for: ${key}`);
     console.dir({ result });
     return result;
   };
